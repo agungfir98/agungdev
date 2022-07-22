906c3ebb5f3bb61abff15a3d6aecbec4b93ec93b
Agung Firmansyah <agungfir98@gmail.com>
2022-07-22 18:44:20 +0700

    Semantic and social list

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,9 +16,9 @@ const Posts: React.FC = () => {
       </Head>
       <div className="min-h-screen flex flex-col gap-y-16">
         <Navbar activeRoute={route} />
-        <div className="h-fit px-6 md:px-0">
+        <main className="h-fit px-6 md:px-0">
           <div className="md:w-6/12 mx-auto flex flex-col gap-y-10">
-            <h1 className="text-3xl font-semibold">About</h1>
+            <h1 className="text-3xl font-semibold">Agung Firmansyah</h1>
             <p className="leading-7 tracking-wide">
               Indonesian-based Electrical Engineer, burning passion in Software
               Development, ReactJS Developer, UI/UX enthusiast, and Graphic
@@ -29,7 +29,7 @@ const Posts: React.FC = () => {
               that you can read it{" "}
               <Link href="/posts/post/my-journey-into-software-development">
                 <a className="text-imperial font-semibold underline underline-offset-1">
-                  here
+                  here.
                 </a>
               </Link>{" "}
               In my spare time I like to play game or listening to science stuff
@@ -46,8 +46,73 @@ const Posts: React.FC = () => {
                 </p>
               </a>
             </Link>
+            <h2 className="text-3xl font-semibold">Social</h2>
+            <ul className="ml-5 flex flex-col gap-y-2" style={{listStyle: "disc"}}>
+              <li>
+                <p className="font-semibold">
+                  Full Name: Agung Firmansyah
+                </p>
+              </li>
+              <li>
+                <p className="font-semibold">
+                  Pronouns: He/Him
+                </p>
+              </li>
+              <li>
+                <p className="font-semibold">
+                  Email:{" "}
+                  <Link href="mailto:agungfir98@gmail.com">
+                    <a
+                      target="_blank"
+                      className="text-imperial font-semibold underline underline-offset-1"
+                    >
+                      agungfir98@gmail.com
+                    </a>
+                  </Link>
+                </p>
+              </li>
+              <li>
+                <p className="font-semibold">
+                  Github:{" "}
+                  <Link href="https://github.com/agungfir98">
+                    <a
+                      target="_blank"
+                      className="text-imperial font-semibold underline underline-offset-1"
+                    >
+                      @agungfir98
+                    </a>
+                  </Link>
+                </p>
+              </li>
+              <li>
+                <p className="font-semibold">
+                  Twitter:{" "}
+                  <Link href="https://twitter.com/agung_firm">
+                    <a
+                      target="_blank"
+                      className="text-imperial font-semibold underline underline-offset-1"
+                    >
+                      @agung_firm
+                    </a>
+                  </Link>
+                </p>
+              </li>
+              <li>
+                <p className="font-semibold">
+                  LinkedIn:{" "}
+                  <Link href="https://linkedin.com/in/agung-firmansyah98">
+                    <a
+                      target="_blank"
+                      className="text-imperial font-semibold underline underline-offset-1"
+                    >
+                      agung-firmansyah98
+                    </a>
+                  </Link>
+                </p>
+              </li>
+            </ul>
           </div>
-        </div>
+        </main>
         <Footer />
       </div>
     </>
